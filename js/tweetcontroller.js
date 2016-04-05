3e1acd774cf7ec802fcc067f419e3c36649801c7
TristanLobaugh <ttrader_atl@yahoo.com>
2016-04-05 16:53:18 -0400

    added api link and styling

--- js/tweetcontroller.js
+++ js/tweetcontroller.js
@@ -1,6 +1,28 @@
-var tweetApp = angular.module("tweetApp", []);
+var tweetApp = angular.module("tweetApp", ["ngRoute"]);
+tweetApp.controller("tweetController", function($scope, $http, $routeParams, $interval){
 
-tweetApp.controller("tweetController", function($scope, $http){
+	$scope.message = "Hello Wrold";
+	var url = "http://www.digitalcrafts.com/students/twitter/hashtag.php?hash=TheMasters&secondHash=AugustaNational";
+
+	$http.get(url).success(function(data){
+		console.log(data.statuses);
+		$scope.data = data.statuses;
+		for(var i = 0; i < $scope.data.length; i++){
+			var time = $scope.data[i].created_at;
+			var tweetTime = new Date(time);
+			$scope.data[i].tweetSeconds = tweetTime.getTime()/1000;
+			console.log(tweetTime);
+
+			$interval(function(){
+				for(var i = 0; i < $scope.data.length; i++){
+					var currentDate = new Date();
+					var currentTimeInSeconds = currentDate.getTime()/1000;
+					$scope.data[i].sinceTweeted = Math.floor((currentTimeInSeconds - $scope.data[i].tweetSeconds)/60);
+				};
+			}, 1000);
+
+		}
+	});
 
 
 });
